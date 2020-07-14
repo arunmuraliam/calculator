6253712ab382f77483419026a22bd7f8c1fa1602
Arun Murali <arunmuraliam.707@gmail.com>
2020-07-13 17:26:32 -0700

    entry widget is added

--- calculator.py
+++ calculator.py
@@ -6,9 +6,19 @@ root = Tk()
 root.title("Calculator")
 root.configure(background="powder blue")
 root.resizable(width=False, height=False)
-root.geometry("416x550+469+60")
+root.geometry("416x550+469+60")   #set the size of frame
+
+# creating a frame
 calc = Frame(root, bd=20, pady=5, bg="gainsboro", relief=RIDGE)
 calc.grid()
+
+
+
+# entry widget is created
+txtDisplay = Entry( calc, font=('arial', 16, 'bold'), bd=20, width=28,justify=RIGHT)
+txtDisplay.grid(row=0, column=0, columnspan=5, pady=1)
+txtDisplay.bind("<Key>", lambda e : "break")  # disable characters from keyboard
+txtDisplay.insert(0, "0")
 
 
 root.mainloop()
